42d6b2d8f941c89bb5d8f5b0cde5ac987b677cd6
Your Name <youremail@example.com>
2025-12-20 02:59:07 +0100

    Fix app router entry files

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,15 +21,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body>
-        <Navbar />
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
